44203c51fa751aa10aed4e5dff31b0f474223e96
Mizuki Nakano <mizukin1187@gmail.com>
2022-03-01 17:46:30 +0900

    added stuff

--- web.py
+++ web.py
@@ -2,10 +2,11 @@ import streamlit as st
 import requests
 
 
-api_url = 'https://cmpapi-7wc6zc723a-ew.a.run.app/'
+api_url = 'https://cmpapi-7wc6zc723a-ew.a.run.app'
+
 
 response = requests.get(api_url)
 
 pred = response.json()
 
-pred
+st.write(pred)
