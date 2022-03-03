2bb63e298bf9509af25b0d9e53da950088c38a98
Mizuki Nakano <mizukin1187@gmail.com>
2022-03-03 17:33:53 +0900

    added stuff

--- web.py
+++ web.py
@@ -12,12 +12,12 @@ from music21 import converter, environment
 # st.write(pred)
 
 #hopoefuly it'll work
-us = environment.UserSettings()
-us['musescoreDirectPNGPath'] = '/build_498a5956/usr/bin/mscore3'
-us['musicxmlPath'] = '/build_498a5956/usr/bin/mscore3'
+# us = environment.UserSettings()
+# us['musescoreDirectPNGPath'] = '/build_498a5956/usr/bin/mscore3'
+# us['musicxmlPath'] = '/build_498a5956/usr/bin/mscore3'
 
 s = converter.parse('tinyNotation: 4/8 C4_hi D-4 E#4 F8 trip{G4 A4 B4} c4')
-s.show()
+s.show('midi')
 
 
 print('success')
