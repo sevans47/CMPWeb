e50b18ddad211db8856ba7eb1c40fb0455454bcb
Mizuki Nakano <mizukin1187@gmail.com>
2022-03-03 12:18:58 +0900

    added stuff

--- web.py
+++ web.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import requests
-import music21
+from music21 import converter
 
-api_url = 'https://cmpapi-7wc6zc723a-ew.a.run.app'
+# api_url = 'https://cmpapi-7wc6zc723a-ew.a.run.app'
 
 
-response = requests.get(api_url)
+# response = requests.get(api_url)
 
-pred = response.json()
+# pred = response.json()
 
-st.write(pred)
+# st.write(pred)
+
+s = converter.parse('tinyNotation: 4/8 C4_hi D-4 E#4 F8 trip{G4 A4 B4} c4')
+st.write(s.show())
