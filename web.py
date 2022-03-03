88173fbf603c958e42bc2df97d6662f9a2194ee8
Mizuki Nakano <mizukin1187@gmail.com>
2022-03-03 15:19:17 +0900

    removed lines

--- web.py
+++ web.py
@@ -12,9 +12,9 @@ from music21 import converter, environment
 # st.write(pred)
 
 #hopoefuly it'll work
-us = environment.UserSettings()
-us['musescoreDirectPNGPath'] = '/app/usr/bin/mscore3'
-us['musicxmlPath'] = '/app/usr/bin/mscore3'
+# us = environment.UserSettings()
+# us['musescoreDirectPNGPath'] = '/usr/bin/mscore3'
+# us['musicxmlPath'] = '/usr/bin/mscore3'
 
 s = converter.parse('tinyNotation: 4/8 C4_hi D-4 E#4 F8 trip{G4 A4 B4} c4')
 s.show('midi')
